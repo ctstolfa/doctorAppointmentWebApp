efac0b6409c77d6f92efeb55a3ec3bb0cb717d9e
limboden <99365731+limboden@users.noreply.github.com>
2022-02-18 17:48:36 -0600

    created and started on account

---
+++ account.py
@@ -0,0 +1,14 @@
+class account:
+
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+
+    def __init__(self, u, p):
+        self.username = u
+        self.password = p
+
+        username = ""
+        password = ""
+        #email = ""?
+    
