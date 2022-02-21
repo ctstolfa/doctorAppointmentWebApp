3369418a8aeff97751f588eaab2d87d27f9b6324
limboden <99365731+limboden@users.noreply.github.com>
2022-02-21 17:28:55 -0600

    changed time zone
    not sure if this is correct syntax

--- DocAppointment/Doc_App/models.py
+++ DocAppointment/Doc_App/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+
+class Patient(models.Model):
+    acc = models.ForeignKey(Account, on_delete=models.CASCADE)
+    # doctor?
+
+
+class Doctor(models.Model):
+    acc = models.ForeignKey(Account, on_delete=models.CASCADE)
+    # list of patients?
